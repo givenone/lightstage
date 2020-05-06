c4a7fb01b9707623ebcf7963f7c09f2dfa8213fc
givenone <givenone1@gmail.com>
2020-05-06 17:54:41 +0900

    add reconstruction & mesh - normal combination

---
+++ run.sh
@@ -0,0 +1,6 @@
+python binary_reconstruction.py -V -format png -path reconstruction_input/
+echo "reconstruction done"
+python generate_pointcloud_ns.py reconstruction_output/diffuse_albedo.png dist0.exr reconstruction_output/syn.tif result/before.ply
+echo "generating ply file done"
+./mesh_opt result/before.ply -lambda 0.01 norm:result/fixed.ply
+echo "mesh & normal combination done"
